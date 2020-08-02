1d207763270151b8fa870fbf059f82abdaa89879
jaewon Lee <jcircle67@gmail.com>
2020-08-02 17:31:34 +0900

    Just practice
    Just practice

---
+++ Twilio_tut/main.py
@@ -0,0 +1,18 @@
+# Download the helper library from https://www.twilio.com/docs/python/install
+from twilio.rest import Client
+
+
+# Your Account Sid and Auth Token from twilio.com/console
+# DANGER! This is insecure. See http://twil.io/secure
+account_sid = 'AC3b673ffc755845f45272d1ddd4c52983'
+auth_token = 'your_auth_token'
+client = Client(account_sid, auth_token)
+
+message = client.messages \
+                .create(
+                     body="Join Earth's mightiest heroes. Like Kevin Bacon.",
+                     from_='+10',
+                     to='+1020358562'
+                 )
+
+print(message.sid)
